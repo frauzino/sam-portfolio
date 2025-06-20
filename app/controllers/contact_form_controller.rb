b4463dbd317a8730586e5e07103bcc9d74450034
Richard Young <richarddfyoung@gmail.com>
2025-06-20 13:25:51 -0400

    changes to text on events and about pages

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -5,6 +5,7 @@ class ContactFormController < ApplicationController
     @email = params[:contact_form][:email]
     @message = params[:contact_form][:message]
 
+    sleep(1)
     flash[:notice] = "Thank you for your message, #{@name}. I will get back to you soon."
     redirect_to root_path
   end
